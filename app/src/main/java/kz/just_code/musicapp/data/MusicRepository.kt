dbd80068406a65a01a36f438197e4ec1befa2a4c
Madina <123440519+sleepingforest100@users.noreply.github.com>
2023-12-05 12:42:51 +0600

    commit

--- app/src/main/java/kz/just_code/musicapp/data/MusicRepository.kt
+++ app/src/main/java/kz/just_code/musicapp/data/MusicRepository.kt
@@ -1,10 +1,7 @@
 package kz.just_code.musicapp.data
 
 import androidx.lifecycle.LiveData
-<<<<<<< HEAD
-import kz.just_code.musicapp.AlbumItem
-=======
->>>>>>> origin/add_network
+
 import kz.just_code.musicapp.PlaylistSecondList
 import kz.just_code.musicapp.SongTrackItem
 
